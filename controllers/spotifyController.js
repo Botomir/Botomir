b46d4a98100062bc24902e5eeb70979ddbcb2e98
SoorajModi <soomodi@gmail.com>
2021-02-22 18:18:14 -0500

    Fix linter issues

--- controllers/spotifyController.js
+++ controllers/spotifyController.js
@@ -1,16 +1,16 @@
 const source = require('rfr');
 
-const {getSpotifyAuthToken} = source('lib/spotify/spotifyApi');
+const { getSpotifyAuthToken } = source('lib/spotify/spotifyApi');
 
 const SpotifyController = {
     get(req, res) {
         const userid = req.query.state;
-        const {code} = req.query;
-        let error = req.query.error || 'Missing the userid and the authentication code';
+        const { code } = req.query;
+        const error = req.query.error || 'Missing the userid and the authentication code';
 
         if (userid && code) {
             getSpotifyAuthToken(userid, code);
-            res.render("authenticate");
+            res.render('authenticate');
         } else {
             res.render('authenticateError', {
                 error,
