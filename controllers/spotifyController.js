7c1277c9472ce22b2a437c09207004f810f947aa
SoorajModi <soomodi@gmail.com>
2021-02-22 18:16:41 -0500

    Add authenticateError and handle error condition in spotifyContoller

--- controllers/spotifyController.js
+++ controllers/spotifyController.js
@@ -1,21 +1,21 @@
 const source = require('rfr');
 
-const { getSpotifyAuthToken } = source('lib/spotify/spotifyApi');
+const {getSpotifyAuthToken} = source('lib/spotify/spotifyApi');
 
 const SpotifyController = {
     get(req, res) {
         const userid = req.query.state;
-        const { code } = req.query;
+        const {code} = req.query;
         let error = req.query.error || 'Missing the userid and the authentication code';
 
         if (userid && code) {
             getSpotifyAuthToken(userid, code);
-            error = null;
+            res.render("authenticate");
+        } else {
+            res.render('authenticateError', {
+                error,
+            });
         }
-
-        return res.render('authenticate', {
-            error,
-        });
     },
 };
 
