309589ea649ddada470f368f5e76d22bc3523fd1
Marshall Asch <maasch@rogers.com>
2021-03-12 21:20:11 -0500

    added the ability to roll a basic 6 sided die

---
+++ bot/commands/utility/diceRoll.js
@@ -0,0 +1,23 @@
+const source = require('rfr');
+const randomNumber = require('pure-random-number')
+
+
+const { sendMessage } = source('bot/utils/util');
+const logger = source('bot/utils/logger');
+
+function diceRoleCommand(message) {
+
+    return randomNumber(1, 6)
+      .then(number => sendMessage(message.channel, `:game_die: ${number}`))
+      .catch(err => logger.error('error getting settings:', err))
+}
+
+module.exports = {
+    args: false,
+    name: 'roll',
+    botAdmin: false,
+    description: 'roles a 6 sided die',
+    usage: '',
+    aliases: [],
+    execute: diceRoleCommand,
+};
