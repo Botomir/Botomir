82395f9fbf808473ee453041ffe2e62bf18b0a70
Marshall Asch <maasch@rogers.com>
2021-03-12 21:35:02 -0500

    can now specify the number of sides on the die

--- bot/commands/utility/diceRoll.js
+++ bot/commands/utility/diceRoll.js
@@ -4,8 +4,12 @@ const randomNumber = require('pure-random-number');
 const { sendMessage } = source('bot/utils/util');
 const logger = source('bot/utils/logger');
 
-function diceRoleCommand(message) {
-    return randomNumber(1, 6)
+function diceRoleCommand(message, args) {
+
+    const sides = Number.parseInt(args[0], 10) || 6;
+
+    if (sides <= 1) return sendMessage(message.channel, `Sorry I can't figure out how to role a ${sides} die.`);
+    return randomNumber(1, sides)
         .then((number) => sendMessage(message.channel, `:game_die: ${number}`))
         .catch((err) => logger.error('error rolling a die:', err));
 }
@@ -14,8 +18,8 @@ module.exports = {
     args: false,
     name: 'roll',
     botAdmin: false,
-    description: 'roles a 6 sided die',
-    usage: '',
+    description: 'roles a 6 sided die, optionally can specify the number of sides',
+    usage: '[<numSides>]',
     aliases: [],
     execute: diceRoleCommand,
 };
