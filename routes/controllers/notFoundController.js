32baa4ff4e3c4f2703bfe969e9f9e1c5871a7dd1
Marshall Asch <maasch@rogers.com>
2021-03-14 18:24:55 -0400

    added log message when the 404 handler is hit

--- routes/controllers/notFoundController.js
+++ routes/controllers/notFoundController.js
@@ -1,5 +1,10 @@
+const source = require('rfr');
+
+const logger = source('bot/utils/logger');
+
 const NotFoundController = {
     get(req, res) {
+        logger.warn(`page not found '${req.path}' is not a valid path`);
         res.render('404');
     },
 };
