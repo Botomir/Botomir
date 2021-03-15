f02c092ae02b8699c642aa7085b43681c9c14096
Marshall Asch <maasch@rogers.com>
2021-03-14 22:41:28 -0400

    404 errors now return a 404 status code

--- routes/controllers/notFoundController.js
+++ routes/controllers/notFoundController.js
@@ -5,7 +5,7 @@ const logger = source('bot/utils/logger');
 const NotFoundController = {
     get(req, res) {
         logger.warn(`page not found '${req.path}' is not a valid path`);
-        res.render('404');
+        res.status(404).render('404');
     },
 };
 
