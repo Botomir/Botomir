dfcbc1d80f902e12226b8c58e88fbd47f02ad498
Marshall Asch <maasch@rogers.com>
2021-04-03 23:46:47 -0400

    changed the util test and updated the logger to not print to the console when running the test envirionment

--- test/util.test.js
+++ test/util.test.js
@@ -1,24 +1,31 @@
 const util = require('../bot/utils/util');
 
-test('should fetch users', () => {
-    const channelSuccess = {
-        send: (m) => {
-            expect(m).toBe('abc');
-            return new Promise(((resolve) => {
-                resolve('success');
-            }));
-        },
-    };
 
-    const channelFail = {
-        send: (m) => {
-            expect(m).toBe('myMessage');
-            return new Promise(((resolve, reject) => {
-                reject(new Error('error'));
-            }));
-        },
-    };
+describe('test sendmessage', () => {
 
-    expect(() => util.sendMessage(channelSuccess, 'abc')).not.toThrow();
-    expect(() => util.sendMessage(channelFail, 'myMessage')).not.toThrow();
+    test('underlying channel.send resolves', async () => {
+        const channelSuccess = {
+            send: (m) => {
+                expect(m).toBe('abc');
+                return new Promise(((resolve) => {
+                    resolve('success');
+                }));
+            },
+        };
+
+        await expect(util.sendMessage(channelSuccess, 'abc')).resolves;
+    });
+
+    test('underlying channel.send rejects', async () => {
+        const channelFail = {
+            send: (m) => {
+                expect(m).toBe('myMessage');
+                return new Promise(((resolve, reject) => {
+                    reject(new Error('error'));
+                }));
+            },
+        };
+
+        await expect(util.sendMessage(channelFail, 'myMessage')).resolves;
+    });
 });
