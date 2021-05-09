24f9110c3c587298d0978e6cbe06f80740b1c4ba
Marshall Asch <maasch@rogers.com>
2021-05-09 17:58:01 -0400

    removed some testing log statemnts I forgot about

--- bot/scanner/messageLinks.js
+++ bot/scanner/messageLinks.js
@@ -7,11 +7,9 @@ const logger = source('bot/utils/logger');
 const messageLinkRegex = /https:\/\/discord.com\/channels\/([0-9]*)\/([0-9]*)\/([0-9]*)/;
 
 function messageLink(message) {
-    logger.info('log');
     const parts = messageLinkRegex.exec(message);
     if (parts === null || parts.length !== 4) return;
 
-    logger.info('matched');
     const serverID = parts[1];
     const channelID = parts[2];
     const messageID = parts[3];
@@ -26,11 +24,11 @@ function messageLink(message) {
             const embededMessage = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setDescription(linkedMessage.content)
-                .setAuthor(linkedMessage.author.username, `https://cdn.discordapp.com/avatars/${linkedMessage.author.id}/${linkedMessage.author.avatar}`);
+                .setAuthor(`${linkedMessage.author.username} in #${linkedMessage.channel.name}`, `https://cdn.discordapp.com/avatars/${linkedMessage.author.id}/${linkedMessage.author.avatar}`);
 
             return sendMessage(message.channel, embededMessage);
         })
-        .catch((err) => logger.error('something went wrong', err));
+        .catch((err) => logger.error('something went wrong with the embeded message', err));
 }
 
 module.exports = {
