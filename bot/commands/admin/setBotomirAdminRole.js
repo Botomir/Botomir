c2fa90a1f804b7183e2e437e500fca1dfd9bc83a
Filip Hasson <filip.hasson@gmail.com>
2021-05-11 17:54:59 -0700

    added set-admin command

---
+++ bot/commands/admin/setBotomirAdminRole.js
@@ -0,0 +1,35 @@
+const source = require('rfr');
+
+const { sendMessage } = source('bot/utils/util');
+
+function setBotomirAdminRole(message, args, config) {
+    return config.setAdminRole(args.join())
+        .save()
+        .then(() => sendMessage(message.channel, `The admin role has been updated to ${args.join()}`))
+        .catch((err) => sendMessage(message.channel, 'Error Something went wrong:', err));
+}
+
+module.exports = {
+    args: 1,
+    name: 'set-botomir-admin-role',
+    botAdmin: true,
+    description: 'change the bot admin role name',
+    usage: '<string>',
+    aliases: ['set-admin'],
+    execute: setBotomirAdminRole,
+    docs: `#### Set command prefix
+- Command: \`set-admin-role-name\`
+- Args:
+    - requires \`<new-role>\` as only argument
+- Returns: Botomir is updated to use the new admin role and a success or failure message is sent
+- Specifications:
+  - Use the ping command to confirm the role name is updated
+- Example usage:
+\`\`\`
+User
+> !set-admin bot-lord
+
+Botomir
+> Settings updated.
+\`\`\``,
+};
