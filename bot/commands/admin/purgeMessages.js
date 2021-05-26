c271eff1ddf0d726a4309853789bb67c294d61d5
Marshall Asch <maasch@rogers.com>
2021-05-26 17:55:57 -0400

    cleaned up delete command

--- bot/commands/admin/purgeMessages.js
+++ bot/commands/admin/purgeMessages.js
@@ -6,54 +6,41 @@ const { Audit, Events } = source('models/audit');
 
 const logger = source('bot/utils/logger');
 
+function auditDelete(message, isSuccess, details) {
+    let event = new Audit()
+        .setGuild(message.guild.id)
+        .setChannel(message.channel.id)
+        .setUser(message.author.id)
+        .setCommand(message.content)
+        .setEvent(Events.BATCH_MESSAGES_DELETED)
+        .setDetails(details);
+
+    event = isSuccess ? event.setSuccess() : event.setFailed();
+
+    event.save()
+        .then(() => logger.info('saved audit event'))
+        .catch((e) => logger.error('failed to save audit event', e));
+}
+
 function purgeCommand(message, args) {
     if (args.length !== 1) {
-        new Audit()
-            .setGuild(message.guild.id)
-            .setChannel(message.channel.id)
-            .setUser(message.author.id)
-            .setCommand(message.content)
-            .setEvent(Events.BATCH_MESSAGES_DELETED)
-            .setFailed()
-            .setDetails('Error you can only give one number for the number of messages to delete')
-            .save()
-            .then(() => logger.info('saved audit event'))
-            .catch((e) => logger.error('failed to save audit event', e));
+        auditDelete(message, false, 'Error you can only give one number for the number of messages to delete');
         return sendMessage(message.channel, 'Error you can only give one number for the number of messages to delete');
     }
 
     const numMessages = Number.parseInt(args[0], 10);
     if (Number.isNaN(numMessages)) {
-        new Audit()
-            .setGuild(message.guild.id)
-            .setChannel(message.channel.id)
-            .setUser(message.author.id)
-            .setCommand(message.content)
-            .setEvent(Events.BATCH_MESSAGES_DELETED)
-            .setFailed()
-            .setDetails('The number of messages to delete must be a number')
-            .save()
-            .then(() => logger.info('saved audit event'))
-            .catch((e) => logger.error('failed to save audit event', e));
+        auditDelete(message, false, 'The number of messages to delete must be a number');
         return sendMessage(message.channel, 'The number of messages to delete must be a number');
     }
 
     return message.channel.bulkDelete(numMessages)
         .then((messages) => {
-            logger.warn(`deleted ${messages.size} from channel ${message.channel.name} in ${message.guild.name} by ${message.author.name}`);
+            logger.warn(`deleted ${messages.size} from channel ${message.channel.name} in ${message.guild.name} by ${message.author.username}`);
 
-            new Audit()
-                .setGuild(message.guild.id)
-                .setChannel(message.channel.id)
-                .setUser(message.author.id)
-                .setCommand(message.content)
-                .setEvent(Events.BATCH_MESSAGES_DELETED)
-                .setSuccess()
-                .save()
-                .then(() => logger.info('saved audit event'))
-                .catch((e) => logger.error('failed to save audit event', e));
+            auditDelete(message, true, `successfully deleted ${messages.size} messages`);
 
-            sendMessage(message.channel, `:skull_crossbones: Poof ${messages.size} messages were successfully deleted by <@${message.author}>`);
+            sendMessage(message.channel, `:skull_crossbones: Poof ${messages.size} messages were successfully deleted by ${message.author.toString()}`);
             return new Statistics()
                 .setGuild(message.guild.id)
                 .setEvent(EventTypes.MESSAGES_DELETED)
@@ -63,17 +50,7 @@ function purgeCommand(message, args) {
                 .catch((e) => logger.error('failed to save statistics', e));
         })
         .catch((e) => {
-            new Audit()
-                .setGuild(message.guild.id)
-                .setChannel(message.channel.id)
-                .setUser(message.author.id)
-                .setCommand(message.content)
-                .setEvent(Events.BATCH_MESSAGES_DELETED)
-                .setFailed()
-                .setDetails(e.message)
-                .save()
-                .then(() => logger.info('saved audit event'))
-                .catch((err) => logger.error('failed to save audit event', err));
+            auditDelete(message, false, e.message);
 
             logger.error('failed to delete messages', e);
             sendMessage(message.channel, `Messages failed to delete - ${e.message}`);
