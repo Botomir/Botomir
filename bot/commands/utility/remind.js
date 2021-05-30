6dd1605e365e34b9dea69b6b42d808a5ed4ed546
Marshall Asch <maasch@rogers.com>
2021-05-30 08:46:56 -0400

    updated docs for the command

--- bot/commands/utility/remind.js
+++ bot/commands/utility/remind.js
@@ -40,15 +40,16 @@ module.exports = {
     args: 2,
     name: 'remind',
     botAdmin: false,
-    description: 'Send a reminder message at a scheduled time.',
+    description: 'Send a reminder message at a scheduled time. The date formats must be specified using this [date formats](http://date.js.org/)',
     usage: '<time for the reminder>\n<some message to be sent>',
     aliases: [],
     execute: remindHandler,
     docs: `#### Remind Message
 - Command: \`remind\`
-- Returns: Schedules a message to be sent later to remind you about this one.
-- This uses [Crontab](https://crontab.guru/) formatted time strings, they are a little bit more
-  confusing to write but they are very powerful and can be very specific.
+- Returns: Schedules a message to be sent at a later time.
+- This uses [date formats](http://date.js.org/) specified to say when the reminder message should be sent.
+  It is mostly able to identify normal english dates but the date must come before the month. The message
+  must be on the second line and can go on for as long as you want.
 - Example usage:
 \`\`\`
 User
@@ -60,6 +61,21 @@ Botomir
 
 ......
 Thursday at 6:
+Botomir
+> Hey @user! Don't forget: look at this later.
+
+https://discord.com/channels/12345/124/098765
+\`\`\`
+\`\`\`
+User
+> !remind in 5:00pm on the 4th of July
+look at this later
+
+Botomir
+> I will send you the reminder on 07/04/2021.
+
+......
+July 4th at 5:
 Botomir
 > Hey @user! Don't forget: look at this later.
 
