4c9d15b2eb303c59941ef1f5beb4dde640c8dff9
Marshall Asch <maasch@rogers.com>
2021-05-31 18:30:10 -0400

    updated delete audit message to work better

--- bot/events/messageDeleteAuditSend.js
+++ bot/events/messageDeleteAuditSend.js
@@ -1,5 +1,6 @@
 const source = require('rfr');
-const { sendMessage } = require('../utils/util');
+
+const { sendMessage } = source('bot/utils/util');
 
 const logger = source('bot/utils/logger');
 const { Settings } = source('models/settings');
@@ -7,25 +8,37 @@ const { Message } = source('models/message');
 
 function auditHandler(message, client) {
     const guild = client.guilds.cache.get(message.guild.id);
-    let messageObject;
+    let channel;
+    let username = 'unknown';
+    let content = 'unknown';
 
     return Message.find(message.guild.id, message.channel.id, message.id)
         .then((m) => {
-            messageObject = m;
+            if (!m) return null;
+
+            content = m.content;
+            return guild.members.fetch(m.authorID);
+        })
+        .then((u) => {
+            if (u) username = u.user.username;
             return Settings.getServerSettings(message.guild.id);
         })
         .then((config) => {
-            if (!config.auditChannel) return;
+            if (!config || !config.auditChannel) {
+                throw new Error('No audit channel is specified for the guild');
+            }
 
-            const channel = guild.channels.cache.get(config.auditChannel);
+            channel = guild.channels.cache.get(config.auditChannel);
             if (!channel) {
                 logger.error(`audit channel for guild ${message.guild.id} does not exist`);
                 return;
             }
-
-            sendMessage(channel, `**DELETED MESSAGE**\nauthor: <@${messageObject.authorID}>\nContent: ${messageObject.content}`);
+            sendMessage(channel, `**DELETED MESSAGE**\nauthor: ${username}\nChannel: <#${message.channel.id}>\nContent: ${content}`);
         })
-        .catch((e) => logger.error('failed to send the delete message to the audit channel', e));
+        .catch((e) => {
+            logger.error('failed to send the delete message to the audit channel');
+            logger.error(e);
+        });
 }
 
 module.exports = {
