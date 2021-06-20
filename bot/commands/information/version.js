10ef712af907bc60cd36ac5557fb98072b5856a3
Marshall Asch <maasch@rogers.com>
2021-06-19 22:02:00 -0400

    added the command to send back the version number / git commit hash of the code that is running

---
+++ bot/commands/information/version.js
@@ -0,0 +1,31 @@
+const source = require('rfr');
+
+const { version } = source('package');
+const { sendMessage } = source('bot/utils/util');
+const commit = (process.env.COMMIT || 'unknown').slice(0, 7);
+
+function versionCommand(message) {
+    sendMessage(message.channel, `Version: \`${version}\`\nBuild: \`${commit}\``);
+}
+
+module.exports = {
+    args: false,
+    name: 'version',
+    botAdmin: false,
+    description: 'Send the running Botomir version information',
+    usage: '',
+    aliases: [],
+    execute: versionCommand,
+    docs: `#### Version
+- Command: \`version\`
+- Returns: a message with the version number
+- Example usage:
+\`\`\`
+User
+> !version
+
+Botomir
+> Version: \`1.2.3\`
+Build: \`4ba590e\`
+\`\`\``,
+};
