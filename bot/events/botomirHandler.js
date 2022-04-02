88c254ac2f9dcf597752a031811789954f650942
Marshall Asch <maasch@rogers.com>
2022-04-02 11:00:10 -0400

    will now send a response any time botomir gets mentioned in a text chat

---
+++ bot/events/botomirHandler.js
@@ -0,0 +1,35 @@
+const _ = require('lodash');
+const source = require('rfr');
+
+const { sendReply } = source('bot/utils/util');
+const logger = source('bot/utils/logger');
+
+const { Responses } = source('models/responses');
+const { Statistics, EventTypes } = source('models/statistics');
+
+const botomirRegex = /botomir/;
+
+const defaultMessage = "Hi, I'm Botomir! Are you talking about me, the worlds greatest Discord bot????";
+
+function botMessage(message) {
+    if (message.guild === null || message.author.bot || !botomirRegex.test(message.content.toLowerCase())) return;
+
+    Responses.findForMode(message.guild.id, 'botomir')
+        .then((res) => {
+            const response = res.length !== 0 ? _.sample(res).message : defaultMessage;
+            return sendReply(message, response);
+        })
+
+        .then(() => new Statistics()
+            .setGuild(message.guild.id)
+            .setEvent(EventTypes.BOTOMIR_MENTION)
+            .save())
+        .then(() => logger.info('statistics saved'))
+        .catch((err) => logger.error('Error getting message:', err));
+}
+
+module.exports = {
+    name: 'message',
+    once: false,
+    execute: botMessage,
+};
