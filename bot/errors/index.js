ffa0fcf9b437544b42f77d86c1920cf417f5145c
Marshall Asch <maasch@rogers.com>
2022-10-23 17:45:47 -0400

    fix: dont send error message every time a command is not found

---
+++ bot/errors/index.js
@@ -0,0 +1,14 @@
+
+class CommandDoesNotExistError extends Error {
+    constructor(command, ...args) {
+        super(...args);
+        this.name = "CommandDoesNotExistError";
+        if (!this.message) {
+            this.message = `Command '${command}' does not exist`;
+        }
+    }
+}
+
+module.exports = {
+    CommandDoesNotExistError,
+}
